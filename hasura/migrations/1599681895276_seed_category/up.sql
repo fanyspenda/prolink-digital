c4d8fd06c3292ebbfcc8e12e43f83ef2d274c507
fanyspenda <fanyervansyah.9c@gmail.com>
2020-09-10 03:11:16 +0700

    build(seeding): add company and category data seed to migration

---
+++ hasura/migrations/1599681895276_seed_category/up.sql
@@ -0,0 +1,5 @@
+INSERT INTO category (id, name) VALUES 
+(1, 'animasi'),
+(2, 'aplikasi'),
+(3, 'desain'),
+(4, 'gim');
---
+++ hasura/migrations/1599682142771_seed_company/up.sql
@@ -0,0 +1,5 @@
+INSERT INTO company (id, name, contact, address, description, category_id) VALUES
+(1, 'CV. Halo Malang', '082123456789', 'Jl. Raya No. 01', 'Kami studio yang berfokus pada bidang broadcasting event di Malang', 2),
+(2, 'Vernon Studio', '082123456788', 'Jl. Raya No. 02', 'Kami studio Game', 4),
+(3, 'Canfast Studio', '082123456722', 'Jl. Raya No. 03', 'Kami studio Desain', 3),
+(4, 'AniMal Studio', '081289098123', 'Jl. Raya No. 04', 'Kami studio Animasi', 1);
