9fb2dbef03624e5c0dc4017cf8405250a3e35118
fanyspenda <fanyervansyah.9c@gmail.com>
2020-09-10 14:03:02 +0700

    build(frontend): add apollo and antd package

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "antd/dist/antd.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const graphqlClient = new ApolloClient({
+	uri: "http://localhost:8080/v1/graphql",
+	headers: {
+		"content-type": "application/json",
+		"x-hasura-admin-secret": "adminsecret",
+	},
+	cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<ApolloProvider client={graphqlClient}>
+			<Component {...pageProps} />;
+		</ApolloProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
