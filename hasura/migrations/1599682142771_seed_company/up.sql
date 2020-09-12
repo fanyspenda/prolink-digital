618dd042b478f8bb62f42ac13d22816a3e80d171
fanyspenda <fanyervansyah.9c@gmail.com>
2020-09-12 23:49:18 +0700

    build(seeding): add more data for company

--- hasura/migrations/1599682142771_seed_company/up.sql
+++ hasura/migrations/1599682142771_seed_company/up.sql
@@ -1,5 +1,7 @@
 INSERT INTO company (id, name, contact, address, description, category_id) VALUES
-(1, 'CV. Halo Malang', '082123456789', 'Jl. Raya No. 01', 'Kami studio yang berfokus pada bidang broadcasting event di Malang', 2),
-(2, 'Vernon Studio', '082123456788', 'Jl. Raya No. 02', 'Kami studio Game', 4),
-(3, 'Canfast Studio', '082123456722', 'Jl. Raya No. 03', 'Kami studio Desain', 3),
-(4, 'AniMal Studio', '081289098123', 'Jl. Raya No. 04', 'Kami studio Animasi', 1);
+(1, 'CV. Halo Probolinggo', '082123456789', 'Jl. Raya No. 01', 'Kami studio yang berfokus pada bidang broadcasting event di Probolinggo.', 2),
+(2, 'Vernon Studio', '082123456788', 'Jl. Raya No. 02', 'Kami studio Game pertama di Probolinggo yang menjadi perwakilan perlombaan pembuatan game se-Jawa Timur.', 4),
+(3, 'Canfast Studio', '082123456722', 'Jl. Raya No. 03', 'Kami studio Desain khusus untuk produk, logo, dan halaman web.', 3),
+(4, 'AniMal Studio', '081289098123', 'Jl. Raya No. 04', 'Kami studio Animasi yang berfokus pada gambar bergerak 3D.', 1),
+(5, 'Nganimasi Studio', '081289098777', 'Jl. Raya No. 12', 'Kami studio Animasi yang berfokus pada gambar bergerak 2D.', 1),
+(7, 'CV. Image Caster', '081289098888', 'Jl. Panglima Sudirman No. 13, Kec. Mayangan', 'Kami studio Animasi yang bikin animasi menggunakan software animasi', 1);
