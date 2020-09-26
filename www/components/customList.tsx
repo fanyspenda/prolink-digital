c88225529270351d6f107e0bd9a1579fe0d0aa61
fanyspenda <fanyervansyah.9c@gmail.com>
2020-09-26 13:59:01 +0700

    feat(list): responsive list

--- www/components/customList.tsx
+++ www/components/customList.tsx
@@ -1,25 +1,25 @@
 // import { companyType } from "types/company";
 import { Space, Layout, Card, Image, Typography } from "antd";
-import { RiPhoneFill, RiBuilding2Fill } from "react-icons/ri";
+import { RiPhoneLine, RiBuilding2Line } from "react-icons/ri";
 import { Company, Category } from "graphqlSchema/types";
 
-const AddressRenderer = (text: string) => {
-	return (
-		<Space>
-			<RiBuilding2Fill />
-			{text}
-		</Space>
-	);
-};
+// const AddressRenderer = (text: string) => {
+// 	return (
+// 		<Space>
+// 			<RiBuilding2Fill />
+// 			{text}
+// 		</Space>
+// 	);
+// };
 
-const ContactRenderer = (text: string) => {
-	return (
-		<Space>
-			<RiPhoneFill />
-			{text}
-		</Space>
-	);
-};
+// const ContactRenderer = (text: string) => {
+// 	return (
+// 		<Space>
+// 			<RiPhoneFill />
+// 			{text}
+// 		</Space>
+// 	);
+// };
 
 type arrayCompany = Omit<Company, "id" | "category_id" | "category"> & {
 	category: Pick<Category, "name" | "__typename">;
@@ -48,29 +48,43 @@ export const ListRenderer = (
 			</p>
 
 			{companies.map((company) => (
-				<div className="flex flex-row items-center h-40 bg-white shadow-md m-2 p-2 rounded-lg border-2 border-white hover:border-orange-300">
-					<div className="w-32 h-32 overflow-hidden flex items-center justify-center border-4">
+				<div
+					className="flex flex-row flex-wrap md:flex-no-wrap justify-center
+				items-center h-auto bg-white 
+				shadow-md m-2 p-2 rounded-lg border-2 
+				border-white md:justify-start hover:border-orange-300"
+				>
+					<div className="w-32 h-32 overflow-hidden flex items-center justify-center">
 						<Image
 							preview={false}
 							src={company.logo_url || "/companyLogo/default.png"}
 						/>
 					</div>
-					<div className="w-full h-32 border-2 border-red-600 flex flex-row justify-between">
-						<div className="flex flex-col justify-between w-48 pl-2 border-4">
+					<div className="w-full h-auto flex flex-wrap flex-row justify-between">
+						<div className="flex flex-col flex-wrap justify-between w-auto pl-2">
 							<Title level={4}>{company.name}</Title>
 							<Paragraph className="text-gray-600">
 								{company.category.name}
 							</Paragraph>
+							<Paragraph
+								ellipsis={{
+									rows: 2,
+								}}
+							>
+								{company.description}
+							</Paragraph>
 						</div>
 
-						<div className="float-right flex flex-col justify-between w-48 border-4">
-							<Paragraph>{company.address}</Paragraph>
-							<Paragraph>{company.contact}</Paragraph>
-						</div>
+						<div className="float-right flex flex-col justify-between w-48 ml-2 text-gray-600">
+							<div className="flex flex-row  items-center">
+								<RiBuilding2Line />
+								<p className="ml-2">{company.address}</p>
+							</div>
 
-						<div className="float-right flex flex-col justify-between w-48 border-4">
-							<Paragraph>{company.address}</Paragraph>
-							<Paragraph>{company.contact}</Paragraph>
+							<div className="flex flex-row items-center">
+								<RiPhoneLine />
+								<p className="ml-2">{company.contact}</p>
+							</div>
 						</div>
 					</div>
 				</div>
