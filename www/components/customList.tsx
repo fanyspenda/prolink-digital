45864f5612922a398179cfe2310966f44e0559f9
fanyspenda <fanyervansyah.9c@gmail.com>
2020-09-28 13:45:10 +0700

    build(dashboard): initial setup

--- www/components/customList.tsx
+++ www/components/customList.tsx
@@ -51,40 +51,50 @@ export const ListRenderer = (
 				<div
 					className="flex flex-row flex-wrap md:flex-no-wrap justify-center
 				items-center h-auto bg-white 
-				shadow-md m-2 p-2 rounded-lg border-2 
+				shadow-md m-2 p-10 rounded-lg border-2 
 				border-white md:justify-start hover:border-orange-300"
 				>
-					<div className="w-32 h-32 overflow-hidden flex items-center justify-center">
+					<div
+						className="flex-1 overflow-hidden flex items-center justify-center"
+						style={{
+							flex: 1,
+						}}
+					>
 						<Image
+							className="w-full"
 							preview={false}
 							src={company.logo_url || "/companyLogo/default.png"}
 						/>
 					</div>
-					<div className="w-full h-auto flex flex-wrap flex-row justify-between">
-						<div className="flex flex-col flex-wrap justify-between w-auto pl-2">
-							<Title level={4}>{company.name}</Title>
-							<Paragraph className="text-gray-600">
-								{company.category.name}
-							</Paragraph>
-							<Paragraph
-								ellipsis={{
-									rows: 2,
-								}}
-							>
-								{company.description}
-							</Paragraph>
+
+					<div
+						className="flex flex-col flex-wrap justify-between max-w-4xl pl-8"
+						style={{
+							flex: 3,
+						}}
+					>
+						<Title level={4}>{company.name}</Title>
+						<Paragraph className="text-gray-600">
+							{company.category.name}
+						</Paragraph>
+						<Paragraph
+							ellipsis={{
+								rows: 2,
+							}}
+						>
+							{company.description}
+						</Paragraph>
+					</div>
+
+					<div className="float-right flex flex-1 flex-col justify-between  ml-2 text-gray-600">
+						<div className="flex flex-row  items-center">
+							<RiBuilding2Line />
+							<p className="ml-2">{company.address}</p>
 						</div>
 
-						<div className="float-right flex flex-col justify-between w-48 ml-2 text-gray-600">
-							<div className="flex flex-row  items-center">
-								<RiBuilding2Line />
-								<p className="ml-2">{company.address}</p>
-							</div>
-
-							<div className="flex flex-row items-center">
-								<RiPhoneLine />
-								<p className="ml-2">{company.contact}</p>
-							</div>
+						<div className="flex flex-row items-center">
+							<RiPhoneLine />
+							<p className="ml-2">{company.contact}</p>
 						</div>
 					</div>
 				</div>
