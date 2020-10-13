8da95d872e5331ddaeeafa5693cd9d68e0d3c5cb
fanyspenda <fanyervansyah.9c@gmail.com>
2020-10-13 21:40:35 +0700

    fix(base-url): change base url from local to deployment url

--- dashboard/environtment.ts
+++ dashboard/environtment.ts
@@ -1,7 +1,7 @@
 export const AUTH0_DOMAIN = "dev-i7afx3za.au.auth0.com";
 export const AUTH0_CLIENT_ID = "KI2sD2pKaKpg05884DefbP4MMpX94gCi";
 export const HASURA_GRAPHQL = "https://vital-liger-80.hasura.app/v1/graphql";
-export const BASE_URL = "http://localhost:4000/";
+export const BASE_URL = "https://prolink-digital-dashboard.vercel.app/";
 export const CLOUDINARY_UPLOAD_URL =
 	"https://api.cloudinary.com/v1_1/dceo8gpwh/image/upload";
 
